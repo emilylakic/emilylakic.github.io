abb29ff87eeff1b0b4154cea7783108961b543c0
Emily Lakic <emilylakic@gmail.com>
2016-09-24 01:34:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function addBlock() {
         elem.innerhtml = "Hi! I'm Emily";
         return false;
     }
+    
+<a href="www.youtube.com" onclick="document.getElementById('inputField').style.display = 'block';">Click me</a>
+
+<div id="inputField" style="display:none;">
+   <input type="text" id="textInput" value="..." />
+</div>
